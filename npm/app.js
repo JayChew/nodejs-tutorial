e639c3faf90f892ff84ac1065a321d69fb004478
Jay Chew <jay.chew@mediacliq.my>
2025-04-15 18:16:39 +0800

    Update changes

---
+++ npm/app.js
@@ -0,0 +1,4 @@
+const lodash = require('lodash'); // Importing the lodash library
+const numbers = [1, 2, 3, 4, 5]; // An array of numbers
+const reversed = lodash.reverse(numbers); // Reversing the array using lodash
+console.log(reversed); // [5, 4, 3, 2, 1]
